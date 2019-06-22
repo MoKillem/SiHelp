027a722eef461c1b74cf277df93d8eeb65a5b3d6
Juliet Li <juliet.li@visagioaustralia.onmicrosoft.com>
2019-06-22 19:14:22 +0800

    'registerProfileUpdateAllFixed'

--- New/django_project/users/forms.py
+++ New/django_project/users/forms.py
@@ -1,29 +1,31 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(widget=forms.fields.TextInput(attrs={'placeholder': 'e.g man@gmail.com'}))
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-    phonenum =forms.CharField(widget=forms.fields.TextInput(attrs={'class': 'form-control bfh-phone' , 'placeholder': 'e.g 0422676960'}), label = "Phone Number", max_length=10)
+    phone = forms.CharField(max_length=30)
     class Meta:
         model = User
-        fields = ['username','email','first_name','last_name','phonenum','password1','password2']
+        fields = ['username','email','first_name','last_name','phone','password1','password2']
 
 
-class TutorRegisterForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.fields.TextInput(attrs={'placeholder': 'e.g man@gmail.com'}))
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-    phonenum =forms.CharField(widget=forms.fields.TextInput(attrs={'class': 'form-control bfh-phone' , 'placeholder': 'e.g 0422676960'}), label = "Phone Number", max_length=10)
-    major = forms.CharField(max_length=30)
-    profpic= forms.ImageField()
+    phone = forms.CharField(max_length=30)
     class Meta:
         model = User
-        fields = ['username','email','first_name','last_name','phonenum','password1','password2','major','profpic']
+        fields = ['username','email','first_name','last_name','phone']
 
-
-
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['identity','pic']
 
     
