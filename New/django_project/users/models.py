7ff02f4ace2ade7eafcef57f2896e0a23ffbf5f4
Juliet Li <juliet.li@visagioaustralia.onmicrosoft.com>
2019-06-22 16:24:16 +0800

    'littleChangeToDb'

--- New/django_project/users/models.py
+++ New/django_project/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     identity = models.BooleanField(default=False)
-    pic = models.ImageField(upload_to = 'profile_pic', default = 'download.jfif')
+    pic = models.ImageField(upload_to = 'profile_pic', default = 'download.png')
 
     def __str__(self):
         return f'{self.user.username}  Profile'
