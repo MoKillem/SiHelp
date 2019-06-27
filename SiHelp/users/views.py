8a73fef8a1efcc1994d6eeaa88c042c3ff1df9fb
Mokillem <galbeedmok@gmail.com>
2019-06-27 17:25:46 +0800

    Host of changes to front end post system + deletion implementation within account profile + ads shown on account page

--- SiHelp/users/views.py
+++ SiHelp/users/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from blog.models import Ad
+from blog.views import PostDeleteView
 import logging
 
 logger = logging.getLogger(__name__)
@@ -42,25 +44,28 @@ def createprofile(request):
 
 @login_required
 def profile(request):
+    user = request.user#acquire current user
+    ads = Ad.objects.filter(author=user).order_by('-date')# Filter ads
     if(request.method) == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, f'Your account information is updated')
-            return redirect('profile')
-        else:
-            print(u_form.errors)
-            print(p_form.errors)
+        if 'Update' in request.post:
+            if u_form.is_valid() and p_form.is_valid():
+                u_form.save()
+                p_form.save()
+                messages.success(request, f'Your account information is updated')
+                return redirect('profile')
+            else:
+                print(u_form.errors)
+                print(p_form.errors)
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance= request.user.profile)
         
     context = {
         'u_form': u_form,
-        'p_form': p_form
+        'p_form': p_form,
+        'ads':ads
     }
     return render(request, 'users/profile.html', context)
 
