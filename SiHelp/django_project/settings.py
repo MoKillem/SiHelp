1b3458e597cec7983d9db9c1ee7ca1ef11caa9e9
Juliet Li <juliet.li@visagioaustralia.onmicrosoft.com>
2019-07-17 00:16:32 +0800

    Password reset done

--- SiHelp/django_project/settings.py
+++ SiHelp/django_project/settings.py
@@ -128,9 +128,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, ' media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "blog-marketplace"
 LOGIN_URL = "loginAcc"
-#Our Email
-EMAIL_USE_TLS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'galbeedmok@gmail.com'
-EMAIL_HOST_PASSWORD = 'Mokillem99'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bbmsjyl@gmail.com'
+EMAIL_HOST_PASSWORD = 'rwtwwtvljlcgyykz'
--- SiHelp/django_project/urls.py
+++ SiHelp/django_project/urls.py
@@ -28,6 +28,10 @@ urlpatterns = [
     path('createprofile/', user_views.createprofile, name = 'createprofile'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'loginAcc'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
-    path('passchange/',user_views.change_password, name='change_password')
+    path('passchange/',user_views.change_password, name='change_password'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name = 'password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = 'users/password_reset_done.html'), name = 'password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'), name = 'password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'users/password_reset_complete.html'), name = 'password_reset_complete')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
